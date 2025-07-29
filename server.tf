facff1d75542aed5683b8e52e48c8507c74a9045
X188589 <extern.christoph.ehscheidt1@porsche.de>
2025-07-29 22:28:13 +0200

    conditional firewall mapping for servers

--- server.tf
+++ server.tf
@@ -102,9 +102,7 @@ resource "hcloud_server" "worker" {
     }
   )
 
-  firewall_ids = [
-    hcloud_firewall.this.id
-  ]
+  firewall_ids = var.talos_public_ipv4_enabled ? [hcloud_firewall.this.id] : null
 
   public_net {
     ipv4_enabled = var.talos_public_ipv4_enabled
