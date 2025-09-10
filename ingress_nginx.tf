04834e6a3356bef7c2580431dcb7554e31bfa043
X188589 <extern.christoph.ehscheidt1@porsche.de>
2025-09-10 14:35:44 +0200

    add mqtt port to loadbalancer

--- ingress_nginx.tf
+++ ingress_nginx.tf
@@ -44,14 +44,14 @@ data "helm_template" "ingress_nginx" {
   ]
 
   values = [
-    yamlencode({
+    yamlencode(merge({
       controller = {
         kind         = var.ingress_nginx_kind
         replicaCount = local.ingress_nginx_replicas
         topologySpreadConstraints = [
           {
-            topologyKey = "kubernetes.io/hostname"
-            maxSkew     = 1
+            topologyKey       = "kubernetes.io/hostname"
+            maxSkew           = 1
             whenUnsatisfiable = (
               local.worker_sum > local.ingress_nginx_replicas ?
               "DoNotSchedule" :
@@ -73,32 +73,32 @@ data "helm_template" "ingress_nginx" {
             type                  = local.ingress_nginx_service_type
             externalTrafficPolicy = var.ingress_nginx_service_external_traffic_policy
           },
-          local.ingress_nginx_service_type == "NodePort" ?
-          {
-            nodePorts = {
-              http  = local.ingress_nginx_service_node_port_http,
-              https = local.ingress_nginx_service_node_port_https
-            }
-          } : {},
+            local.ingress_nginx_service_type == "NodePort" ?
+            {
+              nodePorts = {
+                http  = local.ingress_nginx_service_node_port_http,
+                https = local.ingress_nginx_service_node_port_https
+              }
+            } : {},
 
-          local.ingress_nginx_service_type == "LoadBalancer" ?
-          {
-            annotations = {
-              "load-balancer.hetzner.cloud/algorithm-type"          = var.ingress_load_balancer_algorithm
-              "load-balancer.hetzner.cloud/disable-private-ingress" = true
-              "load-balancer.hetzner.cloud/disable-public-network"  = !var.ingress_load_balancer_public_network_enabled
-              "load-balancer.hetzner.cloud/health-check-interval"   = "${var.ingress_load_balancer_health_check_interval}s"
-              "load-balancer.hetzner.cloud/health-check-retries"    = var.ingress_load_balancer_health_check_retries
-              "load-balancer.hetzner.cloud/health-check-timeout"    = "${var.ingress_load_balancer_health_check_timeout}s"
-              "load-balancer.hetzner.cloud/hostname"                = local.ingress_service_load_balancer_hostname
-              "load-balancer.hetzner.cloud/ipv6-disabled"           = false
-              "load-balancer.hetzner.cloud/location"                = local.ingress_service_load_balancer_location
-              "load-balancer.hetzner.cloud/name"                    = local.ingress_service_load_balancer_name
-              "load-balancer.hetzner.cloud/type"                    = var.ingress_load_balancer_type
-              "load-balancer.hetzner.cloud/use-private-ip"          = true
-              "load-balancer.hetzner.cloud/uses-proxyprotocol"      = true
-            }
-          } : {},
+            local.ingress_nginx_service_type == "LoadBalancer" ?
+            {
+              annotations = {
+                "load-balancer.hetzner.cloud/algorithm-type"          = var.ingress_load_balancer_algorithm
+                "load-balancer.hetzner.cloud/disable-private-ingress" = true
+                "load-balancer.hetzner.cloud/disable-public-network"  = !var.ingress_load_balancer_public_network_enabled
+                "load-balancer.hetzner.cloud/health-check-interval"   = "${var.ingress_load_balancer_health_check_interval}s"
+                "load-balancer.hetzner.cloud/health-check-retries"    = var.ingress_load_balancer_health_check_retries
+                "load-balancer.hetzner.cloud/health-check-timeout"    = "${var.ingress_load_balancer_health_check_timeout}s"
+                "load-balancer.hetzner.cloud/hostname"                = local.ingress_service_load_balancer_hostname
+                "load-balancer.hetzner.cloud/ipv6-disabled"           = false
+                "load-balancer.hetzner.cloud/location"                = local.ingress_service_load_balancer_location
+                "load-balancer.hetzner.cloud/name"                    = local.ingress_service_load_balancer_name
+                "load-balancer.hetzner.cloud/type"                    = var.ingress_load_balancer_type
+                "load-balancer.hetzner.cloud/use-private-ip"          = true
+                "load-balancer.hetzner.cloud/uses-proxyprotocol"      = true
+              }
+            } : {},
 
         )
         config = merge(
@@ -117,8 +117,9 @@ data "helm_template" "ingress_nginx" {
           enabled = true
         }
       }
-    }),
-    yamlencode(var.ingress_nginx_helm_values)
+    },
+      var.ingress_nginx_helm_values)
+    )
   ]
 
   depends_on = [hcloud_load_balancer_network.ingress]
